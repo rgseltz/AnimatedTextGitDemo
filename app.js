90846bbd5926cbcce36e4fe027c90dcf96ef04c0
ryan <ryanjgecelter@gmail.com>
2021-10-10 15:41:23 +0300

    changed js and css to make letters more purple (rgb with a g value of 0) and dark purple background color

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
+    return `rgb(${r},${0},${b})`;
 }
 
 // const h1 = document.querySelector('h1');
